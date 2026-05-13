46b3755f2eecd67c1b7c63bc2f84f7f464dfb6ae
Kian Gokhale <kiangokh2@gmail.com>
2026-05-13 18:24:57 -0400

    ajout image

--- webBiere/camera.js
+++ webBiere/camera.js
@@ -1,14 +1,16 @@
 import {
     FaceDetector,
     FilesetResolver
-} from "https://cdn.jsdelivr.net/npm/@mediapipe/face_detection/task-vision@latest";
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest";
 
 let faceDetector = null;
 let cameraOuverte = false;
+let detectionFace = false;
 
 const video = document.getElementById('camera');
 const bouton = document.getElementById('ouvrirCamera');
-const reslutats = document.getElementById('resultats');
+const resultats = document.getElementById('resultat');
+const image = document.getElementById('image');
 
 bouton.addEventListener('click', ouvrirCamera);
 
@@ -17,7 +19,7 @@ bouton.addEventListener('click', ouvrirCamera);
         const vision = await FilesetResolver.forVisionTasks(
             "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
         );
-        faceDectector = await FaceDetector.createFromOptions(vision, {
+        faceDetector = await FaceDetector.createFromOptions(vision, {
             baseOptions: {
                 modelAssetPath:
                     "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/latest/blaze_face_short_range.tflite"
@@ -48,9 +50,21 @@ bouton.addEventListener('click', ouvrirCamera);
             const maitenant = performance.now();
             const detection = faceDetector.detectForVideo(video,maitenant);
             if(detection.detections.length > 0){
-                resultats.textContent = `Visage détecté avec une confiance de ${Math.round(detection.detections[0].categories[0].score * 100)}%`;
+                detectionFace = true;
+                resultats.textContent = "Visage détecté";
             } else {
+                detectionFace = false;
                 resultats.textContent = "Aucun visage détecté.";
             }
             requestAnimationFrame(detecterVisage);
+            montrerImage();
         }
+
+        function montrerImage(){
+            if(detectionFace){
+                image.style.display = 'block';
+            }else {
+                image.style.display = 'none';
+            }
+        }
+
